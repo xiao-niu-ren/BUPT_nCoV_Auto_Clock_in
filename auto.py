4a9a2f66c621c250af442b5cd4a492830489d09b
xiaoniuren99 <84443886+xiaoniuren99@users.noreply.github.com>
2022-04-02 19:02:43 +0800

    update

--- auto.py
+++ auto.py
@@ -25,64 +25,66 @@ USER_AGENT = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/2010010
 EXECUTION_XPATH = '/html/body/div[1]/div/form/div[5]/input[2]/@value'
 
 # 表单信息
-# 吐槽一下，写柏油的教务系统和疫情填报的前端程序员都tm煞笔
 DATA = {
-	"area":       "北京市+海淀区",
-	"bztcyy":     "",           # 不合前一天同城原因
-	"city":       "北京市",
-	"created":    "1608086660", # 时间戳
-	"csmjry":     "0",          # 近14日内本人/共同居住者是否去过疫情发生场所
-	"date":       "20201216",   # 填报日期
-	"fjsj":       "20200830",   # 返京时间
-	"fxyy":       "",           # 返校原因
-	"glksrq":     "",           # 观测开始实际
-	"gllx":       "",           # 观察场所
-	"gtjzzfjsj":  "",           # 共同居住者返京时间
-	"gwszdd":     "",           # 未给出
-	"ismoved":    "0",          # 当前地点是否与上次在同一城市
-	"jcbhlx":     "",           # 接触人群类型
-	"jcbhrq":     "",           # 接触时间
-	"jchbryfs":   "",           # 接触方式
-	"jcjgqr":     "0",          # 属于正常情况
-	"jcjg":       "",           # 未解释
-	"jcqzrq":     "",           # 未解释
-	"jcwhryfs":   "",           # 接触方式
-	"jhfjhbcc":   "",           # 计划返京航班班次/车次
-	"jhfjjtgj":   "",           # 计划返京交通工具
-	"jhfjrq":     "",           # 计划返京时间
-	"jhfjsftjhb": "0",          # 未给出
-	"jhfjsftjwh": "0",          # 未给出
-	"jrsfqzfy":   "",           # 未给出
-	"jrsfqzys":   "",           # 未给出
-	"mjry":       "0",          # 密切接触人员
-	"province":   "北京市",
-	"qksm":       "",           # 情况说明
-	"remark":     "",           # 其他信息
-	"sfcxtz":     "0",          # 今日是否出现发热，咽痛，干咳，咳痰，乏力，呕吐，腹泻，嗅觉异常，味觉异常
-	"sfcxzysx":   "0",          # 是否有任何与疫情相关的， 值得注意的情况
-	"sfcyglq":    "0",          # 是否处于观察期
-	"sfjcbh":     "0",          # 今日是否接触无症状感染/疑似/确诊人群
-	"sfjchbry":   "0",          # 今日是否接触过近14日内在湖北其他地区（除武汉）活动过的人员
-	"sfjcqz":     "",           # 未解释
-	"sfjcwhry":   "0",          # 是否接触武汉人员
-	"sfsfbh":     "0",          # 未给出
-	"sfsqhzjkk":  "0",          # 未给出
-	"sftjhb":     "0",          # 今日是否到过或者经停湖北其他地区(除武汉)
-	"sftjwh":     "0",          # 今日是否经停武汉
-	"sfxk":       "0",          # 未解释
-	"sfygtjzzfj": "0",          # 家中是否有共同居住者返京
-	"sfyqjzgc":   "",           # 未给出
-	"sfyyjc":     "0",          # 是否到相关医院或门诊检查
-	"sfzx":       "1",          # 是否在校
-	"sqhzjkkys":  "",           # 未给出
-	"szcs":       "",           # 所在地区
-	"szgj":       "",           # 所在国家
-	"szsqsfybl":  "0",          # 所在社区是否有确诊病例
-	"tw":         "2",          # 体温范围
-	"xjzd":       "",           # 现居住地
-	"xkqq":       "",           # 未解释
-	"xwxgymjzqk": "3",          # 疫苗接种情况：3针
-	"zgfxdq":     "0",          # 中高风险地区
+	"area":       "北京市+海淀区",                 	# 地区，中间用+隔开
+	"city":       "北京市",							# 城市
+	"province":   "北京市",							# 所在省份
+	"sfzx":       "1",          					# 是否在校
+	"created":    "1608086660", 					# 时间戳
+
+	"xwxgymjzqk": "2",          					# 疫苗接种情况：2针
+	"csmjry":     "0",          					# 近14日内本人/共同居住者是否去过疫情发生场所
+	"date":       "20201216",   					# 填报日期
+	"fjsj":       "20200830",   					# 返京时间
+	"fxyy":       "",           					# 返校原因
+	"glksrq":     "",           					# 观测开始实际
+	"gllx":       "",           					# 观察场所
+	"gtjzzfjsj":  "",           					# 共同居住者返京时间
+	"gwszdd":     "",           					# 未给出
+	"ismoved":    "0",          					# 当前地点是否与上次在同一城市
+	"zgfxdq":     "0",          					# 中高风险地区
+	"tw":         "2",          					# 体温范围
+	"szsqsfybl":  "0",          					# 所在社区是否有确诊病例
+	"bztcyy":     "",           					# 不在同城原因（今天为什么和昨天不在一个城市）
+	"jcbhlx":     "",           					# 接触人群类型
+	"jcbhrq":     "",           					# 接触时间
+	"jchbryfs":   "",           					# 接触方式
+	"jcjgqr":     "0",          					# 属于正常情况
+	"jcjg":       "",           					# 未解释
+	"jcqzrq":     "",           					# 未解释
+	"jcwhryfs":   "",           					# 接触方式
+	"jhfjhbcc":   "",           					# 计划返京航班班次/车次
+	"jhfjjtgj":   "",           					# 计划返京交通工具
+	"jhfjrq":     "",           					# 计划返京时间
+	"jhfjsftjhb": "0",          					# 未给出
+	"jhfjsftjwh": "0",          					# 未给出
+	"jrsfqzfy":   "",           					# 未给出
+	"jrsfqzys":   "",           					# 未给出
+	"mjry":       "0",          					# 密切接触人员
+	"qksm":       "",           					# 情况说明
+	"remark":     "",           					# 其他信息
+	"sfcxtz":     "0",          					# 今日是否出现发热，咽痛，干咳，咳痰，乏力，呕吐，腹泻，嗅觉异常，味觉异常
+	"sfcxzysx":   "0",          					# 是否有任何与疫情相关的， 值得注意的情况
+	"sfcyglq":    "0",          					# 是否处于观察期
+	"sfjcbh":     "0",          					# 今日是否接触无症状感染/疑似/确诊人群
+	"sfjchbry":   "0",          					# 今日是否接触过近14日内在湖北其他地区（除武汉）活动过的人员
+	"sfjcqz":     "",           					# 未解释
+	"sfjcwhry":   "0",          					# 是否接触武汉人员
+	"sfsfbh":     "0",          					# 未给出
+	"sfsqhzjkk":  "0",          					# 未给出
+	"sftjhb":     "0",          					# 今日是否到过或者经停湖北其他地区(除武汉)
+	"sftjwh":     "0",          					# 今日是否经停武汉
+	"sfxk":       "0",          					# 未解释
+	"sfygtjzzfj": "0",          					# 家中是否有共同居住者返京
+	"sfyqjzgc":   "",           					# 未给出
+	"sfyyjc":     "0",          					# 是否到相关医院或门诊检查
+	"sqhzjkkys":  "",           					# 未给出
+	"szcs":       "",           					# 所在地区
+	"szgj":       "",           					# 所在国家	
+	"xjzd":       "",           					# 现居住地
+	"xkqq":       "",           					# 未解释
+	
+	
 }
 
 ###############################################################################
@@ -107,7 +109,7 @@ SFZX     = os.environ['SFZX']
 # 进行CAS认证, 获取cookie
 ###############################################################################
 
-logging.info('Ready to authorize for %s', USERNAME)
+logging.info('Start authorize ...')
 
 try:
 	# 设置连接
@@ -140,12 +142,13 @@ try:
 	logging.debug('Post %s, responce: %s', LOGIN_URL, responce)
 
 
-	logging.info('Authorize successed')
+	logging.info('Authorize successed !!!')
 
 ###############################################################################
 # 进行填报
 ###############################################################################
 
+	logging.info('Start form ...')
 	# 设置表单数据
 	data = DATA
 	data['created'] = str(round(time.time()))
@@ -160,6 +163,7 @@ try:
 	# 填报
 	responce = session.post(url=FORM_URL, headers=headers, data=data)
 	logging.debug('Post %s, responce: %s', FORM_URL, responce)
+	logging.info('Result below :')
 	logging.info('Responce: %s', responce.json()['m'])
 
 except Exception as e:
