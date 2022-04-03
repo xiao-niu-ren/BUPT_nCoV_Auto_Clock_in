7fe256f12679492a9e0c7588c0012ad9ebd3a7d6
MikasaEureka <37149207+MikasaEureka@users.noreply.github.com>
2022-04-03 17:19:15 +0800

    Update server_bot.py

--- server_bot.py
+++ server_bot.py
@@ -1,8 +1,26 @@
 import time
 import requests,datetime
 
-from constant import *
+# from typing import Optional
+from abc import ABCMeta, abstractmethod
 
+class INotifier(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def PLATFORM_NAME(self) -> str:
+        """
+        将 PLATFORM_NAME 设为类的 Class Variable，内容是通知平台的名字（用于打日志）。
+        如：PLATFORM_NAME = 'Telegram 机器人'
+        :return: 通知平台名
+        """
+    @abstractmethod
+    def notify(self, *, success, msg, data,username, name) -> None:
+        """
+        通过该平台通知用户操作成功的消息。失败时将抛出各种异常。
+        :param success: 表示是否成功
+        :param msg: 成功时表示服务器的返回值，失败时表示失败原因；None 表示没有上述内容
+        :return: None
+        """
 
 class ServerJiangNotifier(INotifier):
     PLATFORM_NAME = 'Server 酱'
